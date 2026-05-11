7d53e02c85c81ff84ece150ae64d5ac601a3cc59
Andrii Dryha <driga.and@gmail.com>
2026-05-11 17:38:16 +0300

    fixing build

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,5 @@
-
-import AnimatedList from "@/components/AnimatedList";
 import Overview from "@/components/overview";
 import GlassContainer from "@/components/surfaces/GlassContainer";
-
-const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'];
-
 
 export default function Home() {
   return (
