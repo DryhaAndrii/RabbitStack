321a9e708ab83e5c4ada2c470453e46fe9d5b5be
Andrii Dryha <driga.and@gmail.com>
2026-05-17 17:28:53 +0300

    test deploy

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <GlassContainer className="w-full max-w-xl">
         <div className="flex flex-col gap-8">
           <h1 className="text-4xl font-semibold tracking-tight text-white sm:text-5xl">
-            Authentication
+            Authentications
           </h1>
 
           <Suspense
